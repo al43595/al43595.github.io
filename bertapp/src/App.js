5e8b345acc24b60e44c6c4b96db5a4342f9c3b4f
Albert Lu <albertlu@utexas.edu>
2023-10-13 01:18:17 -0500

    reactapp

---
+++ bertapp/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          You are the main character to your own story
+        </p>
+        <a
+          className="App-link"
+          href="albertlu.studio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Portfolio
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Projects.js
@@ -0,0 +1,19 @@
+// src/components/Projects.js
+import React from 'react';
+import ProjectItem from './ProjectItem';
+
+class Projects extends React.Component {
+    state = {
+        projects: [/* ... your hardcoded projects here ... */]
+    };
+
+    render() {
+        return (
+            <div>
+                {this.state.projects.map(project => <ProjectItem key={project.id} details={project} />)}
+            </div>
+        );
+    }
+}
+
+export default Projects;
