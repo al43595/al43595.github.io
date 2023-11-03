f04e652baed38a59266136dceac38eb7a6ab585d
Albert Lu <albertlu@utexas.edu>
2023-11-03 00:35:29 -0500

    fixed backend and frontent

--- bertapp/src/App.js
+++ bertapp/src/App.js
@@ -17,9 +17,26 @@ function App() {
         >
           View Portfolio
         </a>
+        <button onClick={() => joinProject('some_project_id')} style={{ marginTop: '20px' }}>
+          Join Project
+        </button>
       </header>
+      
     </div>
   );
 }
+
+function joinProject(projectId) {
+  fetch(`http://127.0.0.1:5000/join/${projectId}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      alert(data.message);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
 
 export default App;
